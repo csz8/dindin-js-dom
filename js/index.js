ad658e5fec9d110e47f8d48bad56263f65e78707
Célio Soares <cszenc@gmail.com>
2022-03-24 20:31:20 -0300

    adicionando arquivo js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//GERAL
+
 // 1. O header deve ter cor de fundo #2E948A
 
 let header = document.getElementsByTagName('header');
@@ -13,30 +15,83 @@ console.log(linkCursos);
 
 linkCursos.setAttribute('href', 'curso.html');
 
-// HOME 2. Na etapa de depoimentos, o título deveria ser "O que falam sobre nós".
+//HOME
+
+// 1. No banner da home, está faltando centralizar os elementos filhos da section que tem o id#introducao.
+
+let introducao = document.getElementById('introducao');
+console.log(introducao);
+
+introducao.style.justifyContent = 'center';
+
+// 2. Na etapa de depoimentos, o título deveria ser "O que falam sobre nós".
 
 let depoimentos = document.querySelector('.titulo.depoimento h3');
 console.log(depoimentos);
 
 depoimentos.innerHTML = 'O que falam sobre nós';
 
-// HOME 1. No banner da home, está faltando centralizar os elementos filhos da section que tem o id#introducao.
 
-let introducao = document.getElementById('introducao');
-console.log(introducao);
-
-introducao.style.flex.justifyContent = 'center';
-
-// HOME 3. Na etapa de blog, o título deveria ser "Mais conteúdo pra você".
+// 3. Na etapa de blog, o título deveria ser "Mais conteúdo pra você".
 
 let blog = document.querySelector('main section:nth-child(5) h3');
 console.log(blog);
 
 blog.innerHTML = 'Mais conteúdo pra você';
 
-// HOME 4. Todos os textos que estiverem com a classe.titulo devem apresentar todas as letras maiúsculas.
+// 4. Todos os textos que estiverem com a classe.titulo devem apresentar todas as letras maiúsculas.
+''
+let titulo = document.querySelectorAll('.titulo');
+console.log(titulo);
 
-let titulos = document.getElementsByClassName('.titulo');
-console.log(titulos)
+titulo.forEach(titulo => {
+    titulo.style.textTransform = 'uppercase';
+});
 
-//usar foreach
+// 5. o botão "ver todos os posts" deve ter um link que direciona para o arquivo blog.htmL
+
+let btnposts = document.getElementById('todos_posts');
+console.log(btnposts);
+
+btnposts.setAttribute('href', 'blog.html');
+
+// 6. Adicionar um novo curso na section que contém o id investimentos_poupando_independencia
+
+let cursoatual = document.querySelector('#investimentos_poupando_independencia');
+console.log(cursoatual);
+
+let novocurso =
+    `<div id="independencia"><img src="/imagens/independencia_financeira.png"
+width="180px" alt="Independência Financeira">
+<h2>Independência Financeira</h2>
+<p>Duis aute irure dolor in reprehenderit in voluptate
+velit esse cillum dolore
+eu fugiat nulla pariatur. </p>
+<a class="comecar_agora" href="./curso.html">começar
+agora</a>
+</div>`;
+
+cursoatual.innerHTML = cursoatual.innerHTML + novocurso;
+
+//CONTATO
+
+// 1. O formulário não está funcionando, o atributo action deve mandar para url sucesso.html
+
+let contato = document.getElementsByTagName('form');
+console.log(contato);
+
+contato.setAttribute('action', 'sucesso.html')
+
+
+// 2. Após o campo de email, precisamos de um novo campo para que o usuário adicione também um número de telefone.
+
+
+
+// 3. O campo de mensagem está ultrapassando o tamanho do elemento pai. Podemos resolver adicionando o atributo box-sizing:border-box;
+
+
+
+// 4. O botão não está do tamanho adequado, precisa ter uma largura maior;
+
+
+// 5. Abaixo da section do formulário, adicione uma seção de comentário igual a página Home.
